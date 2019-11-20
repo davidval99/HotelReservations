1fece53ff3a3d09a9f261ac37011d3fc51493dd7
Sebastián <sebas.mqt@gmail.com>
2019-11-19 19:23:13 -0600

    se borran bibliotecas inutilizadas

--- proyecto_diseno/src/main/java/com/example/proyecto/Controllers/ImageController.java
+++ proyecto_diseno/src/main/java/com/example/proyecto/Controllers/ImageController.java
@@ -6,9 +6,6 @@ import com.example.proyecto.services.ImageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.awt.*;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.util.ArrayList;
 
 @RestController
